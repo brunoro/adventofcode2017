699ce512335956e700db10e5363feffcf5af2810
Gustavo Brunoro <git@hitnail.net>
2017-12-03 03:56:06 -0200

    day 2

---
+++ 2/checksum.hs
@@ -0,0 +1,29 @@
+import System.IO
+import Data.Array
+import qualified Data.Text as T
+import Debug.Trace (trace)
+
+readLines :: FilePath -> IO [T.Text]
+readLines = fmap T.lines . fmap T.pack . readFile
+
+parseInt :: T.Text -> Int
+parseInt x = read (T.unpack x) :: Int
+
+parseLine :: T.Text -> [Int]
+parseLine = map parseInt . T.words
+
+_minMax :: (Int, Int) -> Int -> (Int, Int)
+_minMax (low, high) x = (min low x, max high x)
+
+-- the clean way to implement this would be using Maybe Int, 
+-- but I know for sure that my input won't have big numbers
+minMax :: [Int] -> (Int, Int)
+minMax = foldl _minMax (99999, 0)
+
+sub :: (Int, Int) -> Int
+sub (x, y) = y - x
+
+main :: IO ()
+main = do
+  lines <- readLines "input.txt"
+  print $ foldl (+) 0 $ map (sub . minMax . parseLine) $ lines
